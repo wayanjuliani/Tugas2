8ae4a01b167bfcd7eee68525b2d4eb24d4eeb78d
wayanjuliani <38775967+wayanjuliani@users.noreply.github.com>
2018-04-29 20:32:20 +0800

    Tugas 1 (29/4/2018)
    praktek pull, create file dari github kemudian pull dari gitkraken

---
+++ catatan.php
@@ -0,0 +1,5 @@
+Ini adalah tugas 1 pada pertemuan ke 2 pada hari minggu, 29 April 2018.
+Mencoba pull dan push folder dan file ke github melalui gitkraken. 
+File ini adalah praktek untuk pull
+
+by juliani
